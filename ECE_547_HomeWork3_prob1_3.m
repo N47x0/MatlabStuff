6f0eb85eb02ca0dcd8e737bc1f094aa5250a563b
N47x0 <iggy.soriano@gmail.com>
2015-08-28 09:02:22 -0500

    Cp correction (ones(4) to eye(4))

--- ECE_547_HomeWork3_prob1_3.m
+++ ECE_547_HomeWork3_prob1_3.m
@@ -34,8 +34,8 @@ else
         R = 1;
         xeig=[];
         qq = logspace(-3,2,100);
-        w = logspace(-2,2,100);
-        t = linspace(0,1);
+        w = logspace(-3,2,100);
+        t = linspace(0,1,100);
         
         wn = 11;    %Hz for the actuator natural freq.
         ze = 0.707; %Damping ratio for second order actuator model.
@@ -46,7 +46,7 @@ else
               zeros(1,2) -wn*wn -2*ze*wn];
         Bp = [0;0;0;wn*wn];
         Cp = [C D 0;
-              ones(4)];
+              eye(4)];
         Dp = [zeros(size(Cp,1),1)];
         
          % Form Common Controller (Static Part)
@@ -81,39 +81,45 @@ else
             % Form Loop gain at the input
             L = ss(lgin(Ap,Bp,Cp,Dp,Ac,Bc1,Cc,Dc1));
             
-            % Obtain margins and crossover freq.
             [re,im] = nyquist(L,w);
+            [mag,phase,w] = bode(L);
             
-            wc = Wgm;
+            retDiff = sigma(L, w, 2);
+            stabRob = sigma(L, w, 3);
+            
+            % Obtain margins and crossover freq.
+            
+            
+            %wc = Wgm;
             
             
             % Generate Analysis metrics
-            %mag = abs(L);
-            stabRob = ones(size(L))+ones(size(L))./L;
-            minStabRob = min(abs(stabRob));
-            retDiff = ones(size(L))+L;
-            minRetDiff = min(abs(retDiff));
-            %mag_dB = 20.*log10(mag)';
-            %wc = crosst(mag_dB, w);
-            wiggle_sys = ss(A_cl, B_cl, C_cl, D_cl);
-            y = step(wiggle_sys, t);
-            az = y(:,1);
-            aze = abs(ones(size(az))-az);
-            tau_r = 0;
-            tau_s = 0;
-            fv = aze(prod(size(aze)));
-            e_n = aze - fv*ones(size(aze)) - 0.36*ones(size(aze));
-            e_n1= abs(e_n)+e_n;
-            tau_r = crosst(e_n1,t);
-            e_n = aze - fv*ones(size(aze)) - 0.05*ones(size(aze));
-            e_n1 = abs(e_n) + e_n;
-            tau_s = crosst(e_n1,t);
-            azmin = (abs(min(az)))*100;
-            azmax = (abs(max(az))-1)*100;
-            dmax = rtd*max(abs(y(:,4)))*32.174;
-            ddmax = rtd*max(abs(y(:,5)))*32.174;
-            metric=[ qq(i) minRetDiff minStabRob wc tau_r tau_s azmin azmax dmax ddmax];
-            data(i,:) = metric;
+            %mag = sqrt((re(1,:)).^2 + (im(1,:)).^2);
+            %stabRob = ones(size(L))+ones(size(L))./L;
+%             minStabRob = min(abs(stabRob));
+%             %retDiff = ones(size(L))+L;
+%             minRetDiff = min(abs(retDiff));
+%             mag_dB = 20.*log10(mag)';
+%             wc = crosst(mag_dB, w);
+%             wiggle_sys = ss(A_cl, B_cl, C_cl, D_cl);
+%             y = step(wiggle_sys, t);
+%             az = y(:,1);
+%             aze = abs(ones(size(az))-az);
+%             tau_r = 0;
+%             tau_s = 0;
+%             fv = aze(prod(size(aze)));
+%             e_n = aze - fv*ones(size(aze)) - 0.36*ones(size(aze));
+%             e_n1= abs(e_n)+e_n;
+%             tau_r = crosst(e_n1,t);
+%             e_n = aze - fv*ones(size(aze)) - 0.05*ones(size(aze));
+%             e_n1 = abs(e_n) + e_n;
+%             tau_s = crosst(e_n1,t);
+%             azmin = (abs(min(az)))*100;
+%             azmax = (abs(max(az))-1)*100;
+%             dmax = rtd*max(abs(y(:,4)))*32.174;
+%             ddmax = rtd*max(abs(y(:,5)))*32.174;
+%             metric=[ qq(i) minRetDiff minStabRob wc tau_r tau_s azmin azmax dmax ddmax];
+%             data(i,:) = metric;
             
             figure(1)
             plot(real(xeig),imag(xeig), '*')
